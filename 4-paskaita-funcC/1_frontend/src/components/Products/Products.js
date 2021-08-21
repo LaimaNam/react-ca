9e46ac94674c44688ebcadb9f229dc532f815138
LaimaNam <laima.namajunaite@gmail.com>
2021-08-21 15:46:50 +0300

    small changes

--- 4-paskaita-funcC/1_frontend/src/components/Products/Products.js
+++ 4-paskaita-funcC/1_frontend/src/components/Products/Products.js
@@ -1,33 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import Product from './Product';
+import React, { useEffect, useState } from "react";
+import Product from "./Product";
+import ProductView from "./ProductView";
 
 const Products = ({ heading }) => {
   const [productItems, setProductItems] = useState([]);
-  const [productId, setProductId] = useState('');
+  const [productId, setProductId] = useState("");
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
+    fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
       .then((data) => setProductItems(data));
   }, []);
 
   //functions
   const selectProduct = (id) => {
-    if (productId === '') {
+    if (productId === "") {
       setProductId(id);
     } else {
-      setProductId('');
+      setProductId("");
     }
   };
 
-  return (
-    <>
-      <h2>{heading}</h2>
-      <div className="products-section">
-        {productItems
-          //first time productId state is empty string "", so rendering all products, when clicking on product img stateId becomes product id and showing one product
-          .filter((product) => productId === '' || product.id === productId)
-          .map((product) => (
+  const findProductById = (products, id) => {
+    let product = products.filter((product) => product.id === id);
+    return product[0];
+  };
+
+  if (productId === "") {
+    return (
+      <>
+        <h2>{heading}</h2>
+        <div className="products-section">
+          {productItems.map((product) => (
             <Product
               action={selectProduct}
               key={product.id}
@@ -38,9 +42,27 @@ const Products = ({ heading }) => {
               imgPrice={product.price}
             />
           ))}
-      </div>
-    </>
-  );
+        </div>
+      </>
+    );
+  } else {
+    const product = findProductById(productItems, productId);
+    return (
+      <>
+        <ProductView
+          action={selectProduct}
+          key={product.id}
+          id={product.id}
+          imgSrc={product.image}
+          imgAlt={product.title}
+          category={product.category}
+          imgTitle={product.title}
+          description={product.description}
+          imgPrice={product.price}
+        />
+      </>
+    );
+  }
 };
 
 export default Products;
