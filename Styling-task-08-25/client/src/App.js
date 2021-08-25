c768d0ad2389b5c1ad7085d372a0f26ef904e116
LaimaNam <laima.namajunaite@gmail.com>
2021-08-25 15:57:40 +0300

    styling task initial commit

---
+++ Styling-task-08-25/client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Product from './components/Styled-components/Product';
+
+function App() {
+  return (
+    <>
+      <header></header>
+      <main>
+        <Product />
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
